4f5ce252ac5fce8722338ab5700d48b07078e47c
Martina Kafadar <124434047+Martina-Kafa@users.noreply.github.com>
2023-02-12 14:43:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//potrebno napraviti funkciju kojom nestaje preloader
+
+// window.addEventListener("load", () => {
+//   const preload = document.querySelector(".preload");
+//   preload.classList.add("preload-finish");
+// });
